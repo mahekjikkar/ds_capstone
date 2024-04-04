8d77ca91be6c2f3ea2599d1bafe83b0c5a85960e
Prasham Shah <69897877+Prasham27@users.noreply.github.com>
2024-04-04 17:31:54 +0530

    Update entry_manager.cpp
    Defining the function optimizeInitialRandomPeople for the optimization of first M/2 people

--- entry_manager.cpp
+++ entry_manager.cpp
@@ -14,7 +14,7 @@ private:
     vector<queue<int>> entryGates;
 
 public:
-    Stadium() 
+    Stadium()
     {
         cout<< "Enter the total number of gates in the stadium"<<endl;
         cin >> N;
@@ -32,6 +32,7 @@ public:
         for (int i = 1; i <= M/2; ++i) {
             int gateIndex = rand() % N; // Randomly select a gate index
             entryGates[gateIndex].push(i); // Assign the person to the selected gate
+        }
 
         // Printing the waiting time of each queue before switching (optimization)
         cout << "Displaying waiting time of gates before optimization" << endl;
@@ -47,5 +48,25 @@ public:
             cout<<"Waiting time for gate " << i+1 << " = " << entryGates[i].size()*p << endl;            
         }
 
-    }
+ }
+//This function will balance the first M/2 randomized people into ordered queues by popping people from the larger queue and pushing them into shorter queue according to the average calculated
+void optimizeInitialRandomPeople(){
+    int idealPeoplePerGate = M / (2 * N);    //Calculating average of first M/2 randomised people for balancing
+        
+        for (int i = 0; i < N; ++i) {
+            while (entryGates[i].size() > idealPeoplePerGate) {
+                int person = entryGates[i].back(); 
+                entryGates[i].pop(); //Popping the back person from the current gate
+                for (int j = 0; j < N; ++j) {
+                    //Finding the destination gate (by iterating through each gate excluding the current gate)
+                    if (j != i && entryGates[j].size() < idealPeoplePerGate) {
+                        entryGates[j].push(person);  //Pushing the back person to a destined smaller gate
+                        break;
+                    }
+                }
+            }
+        }
+
+}
+};
 
