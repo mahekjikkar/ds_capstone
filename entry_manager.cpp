e0577d97d9c9eec52bc2b6f1684afdfcba7accfc
Prasham Shah <69897877+Prasham27@users.noreply.github.com>
2024-04-06 22:09:30 +0530

    Update entry_manager.cpp
    Corrected the optimiseInitialRandomPeople function for more edge cases

--- entry_manager.cpp
+++ entry_manager.cpp
@@ -47,31 +47,29 @@ public:
  }
 
 //This function will balance the first M/2 randomised people into ordered queues by popping people from larger queue and pushing them into shorter queue according to the average calculated
-void optimizeInitialRandomPeople(){
-    
-    float idealPeoplePerGate = M / (2 * N);    //Calculating average of first M/2 randomised people for balancing
+void optimizeInitialRandomPeople() {
+        float idealPeoplePerGate = (float)M / (2 * N); // Calculating average of first M/2 randomised people for balancing
 
-    int roundedIdeal = idealPeoplePerGate;
-    float diff = idealPeoplePerGate - (int)idealPeoplePerGate;
-
-    //Rounding off to correctly adjust the people according to
-    if(diff>=0.5)
-    ++roundedIdeal;
-        
         for (int i = 0; i < N; ++i) {
-            while (entryGates[i].size() > roundedIdeal) {
+            while (entryGates[i].size() > idealPeoplePerGate) {
                 int person = entryGates[i].back();
-                entryGates[i].pop(); //Popping the back person from the current gate
-                for (int j = 0; j < N; ++j) {
-                    //Finding the destination gate (by iterating through each gate and excluding the current gate)
-                    if (j != i && entryGates[j].size() <= roundedIdeal) {
-                        entryGates[j].push(1); //Pushing the back person to the destination (smaller) gate
+                entryGates[i].pop();
+                int j = (i + 1) % N; // Start checking from the next gate
+                while (j != i) {
+                    if (entryGates[j].size() < idealPeoplePerGate) {
+                        entryGates[j].push(person);
                         break;
                     }
+                    j = (j + 1) % N;
+                }
+                if (j == i) {
+                    entryGates[i].push(person); // If no shorter queue found, push the person back to the original queue
+                    break;
                 }
             }
         }
-}
+    }
+
 
 // After balancing M/2 people now adding another M/2 people gate-wise 
 void addingNewPeople(){
